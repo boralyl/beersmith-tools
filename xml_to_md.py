7e5e8763013f7c0cb842678907d56e8876760fd7
Aaron Godfrey <me@aarongodfrey.dev>
2020-12-07 20:29:56 -0800

    Added notes, grain % and front matter.

--- xml_to_md.py
+++ xml_to_md.py
@@ -20,15 +20,21 @@ def main(beer_xml_path: str):
     parser = Parser()
     recipe = parser.parse(beer_xml_path)[0]
     ingredients_rows = []
+    total_fermentables_in_kg = sum([f.amount for f in recipe.fermentables])
     for f in recipe.fermentables:
-        ingredients_rows.append(f"|{f.display_amount}|{f.name}|{f.type}|")
+        percent = round((f.amount / total_fermentables_in_kg) * 100, 1)
+        ingredients_rows.append(f"|{f.display_amount}|{f.name}|{f.type}|{percent}%|")
     for h in recipe.hops:
         ingredients_rows.append(
-            f"|{h.display_amount}|{h.name} [{h.alpha}%] - {hop_use(h.use)} {h.display_time}|Hop|"
+            f"|{h.display_amount}|{h.name} [{h.alpha}%] - {hop_use(h.use)} {h.display_time}|Hop|-|"
         )
     for y in recipe.yeasts:
         ingredients_rows.append(
-            f"|1 pkg|{y.name} ({y.laboratory}#{y.product_id})|Yeast|"
+            f"|1 pkg|{y.name} ({y.laboratory}#{y.product_id})|Yeast|-|"
+        )
+    for m in recipe.miscs:
+        ingredients_rows.append(
+            f"|{m.display_amount}|{m.name} ({m.use} {m.display_time})|{m.type}|-|"
         )
     ingredients = "\n".join(ingredients_rows)
 
@@ -39,11 +45,22 @@ def main(beer_xml_path: str):
         )
     mash_step_str = "\n".join(mash_steps)
     md = f"""
+---
+title: "{recipe.name} ({recipe.style.name} | {int(recipe.style.category_number)}{recipe.style.style_letter})"
+collection: recipes
+categories:
+  - Recipes
+tags:
+  - {recipe.style.name.lower()}
+  - {int(recipe.style.category_number)}{recipe.style.style_letter}
+---
+
 **Name**: {recipe.name}<br />
 **Style**: {recipe.style.name} ({int(recipe.style.category_number)}{recipe.style.style_letter})<br />
 **Type**: {recipe.type}
 
 ## Recipe Specifications
+
 **Boil Size**: {recipe.display_boil_size}<br />
 **Batch Size (fermenter)**: {recipe.display_batch_size}<br />
 **Estimated OG**: {recipe.est_og}<br />
@@ -54,8 +71,9 @@ def main(beer_xml_path: str):
 **Boil Time**: {int(recipe.boil_time)} minutes<br />
 
 ## Ingredients
-|Amount|Name|Type|
-|-|-|-|
+
+|Amount|Name|Type|%|
+|-|-|-|-|
 {ingredients}
 
 ## Mash
@@ -65,6 +83,10 @@ def main(beer_xml_path: str):
 |Name|Description|Step Temperature|Step Time|
 |-|-|-|-|
 {mash_step_str}
+
+## Notes
+
+{recipe.notes}
     """
     return md
 
